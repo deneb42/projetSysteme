a76ab164cb7841855bddd4cd3f54b74d7840ba37
deneb <drbenj@free.fr>
2011-06-27 13:05:54 +0200

    cleaning continue, removing warning due to the ofset of 64 bits

--- param.h
+++
@@ -1,9 +0,0 @@
-/*  Find					param.h					By : deneb */
-
-#ifndef PARAM_H
-#define PARAM_H
-
-	int getParam(int* nbPath, int** tab, char*** param, int* minDepth, int* maxDepth, char* argv[], int argc);
-	void freeParam(int tab[], char* param[], int nbParam);
-
-#endif
